816642a8204a3e461c771e01fd51acafd158b5c4
itshahin111 <itshahin111@gmail.com>
2024-12-12 18:34:37 +0600

    More Added

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,62 @@
 <script setup>
-import { ref, onMounted, nextTick } from "vue";
-const items = ref([
-  "https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1467&q=80",
-  "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-  "https://images.unsplash.com/photo-1558882224-dda166733046?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80",
-  "https://images.unsplash.com/photo-1562664377-709f2c337eb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-  "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80",
+import { reactive } from "vue";
+const saarcCountries = reactive([
+  { name: "Bangladesh", capital: "Dhaka" },
+  { name: "Nepal", capital: "Kathmandu" },
+  { name: "Bhutan", capital: "Thimphu" },
+  { name: "Sri Lanka", capital: "Colombo" },
+  { name: "India", capital: "New Delhi" },
+  { name: "Pakistan", capital: "Islamabad" },
+  { name: "Maldives", capital: "Male" },
 ]);
 
-let carousel = null;
+const capitals = [
+  "Dhaka",
+  "Kathmandu",
+  "Thimphu",
+  "Colombo",
+  "New Delhi",
+  "Islamabad",
+  "Male",
+];
 
-const newItem = ref(
-  "https://images.unsplash.com/photo-1682685794761-c8e7b2347702?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
-);
-
-function addNewitem() {
-  items.value.push(newItem.value);
-  carousel.destroy();
-  nextTick(function () {
-    carousel = new Flickity("#carousel", {});
-  });
-  // carousel = new Flickity('#carousel',{})
+function getRandomCapitals() {
+  return capitals;
+  //return capitals.sort(()=>Math.random() - 0.5)
 }
 
-onMounted(() => {
-  carousel = new Flickity("#carousel", {});
-});
+function getScore() {
+  let score = 0;
+  saarcCountries.forEach((country) => {
+    if (country.answer === country.capital) {
+      score++;
+    }
+  });
+  return score;
+}
 </script>
 
 <template>
-  <div class="my-10">
-    <input type="text" v-model="newItem" />
-    <button
-      @click="addNewitem()"
-      class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Button
-    </button>
-  </div>
-  <div class="mx-auto items" id="carousel">
+  <section class="mx-auto container text-left">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="mb-10">{{ saarcCountries }}</p>
+    <p>Your Score: {{ getScore() }}</p>
     <div
-      :style="`background-image:url(${item})`"
-      class="item"
-      v-for="(item, index) in items"
-      :key="item"
+      class="my-5 border border-gray-400 p-5"
+      v-for="(country, index) in saarcCountries"
+      :key="country.name"
     >
-      {{ index + 1 }}
+      <p>What is the capital of {{ country.name }}?</p>
+      <template v-for="capital in capitals">
+        <input
+          :name="country.name"
+          type="radio"
+          :value="capital"
+          v-model="country.answer"
+        />
+        <label class="ml-2 mr-2">{{ capital }}</label>
+      </template>
     </div>
-  </div>
+  </section>
 </template>
-
-<style scoped>
-.items {
-  width: 600px;
-  height: 400px;
-}
-
-.item {
-  width: 600px;
-  height: 400px;
-  background-color: #ccc;
-  background-size: cover;
-}
-</style>
+<style scoped></style>
