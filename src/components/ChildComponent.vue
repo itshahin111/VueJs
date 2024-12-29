cfcbd16ce2e901eef30cd88b4ac6f3cdb02a6447
itshahin111 <itshahin111@gmail.com>
2024-12-29 22:46:56 +0600

    more comp added

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+    <h2 class="my-2 text-xl">Child Component</h2>
+    <p class="text-gray-500">This is a child component</p>
+</template>
+<style></style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ChildComponent from './ChildComponent.vue';
+</script>
+<template>
+    <h1 class="my-5 text-2xl">Parent Component</h1>
+    <p>This is a parent component</p>
+    <ChildComponent/>
+    <ChildComponent/>
+    <ChildComponent/>
+    <ChildComponent/>
+    <ChildComponent/>
+</template>
